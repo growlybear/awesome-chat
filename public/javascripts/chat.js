6e762cc6edef7c1fd73647d0b4ca11da59b56772
Michael Allan <michaelallan@optushome.com.au>
2013-03-11 23:09:10 +1100

    client and server js

---
+++ public/javascripts/chat.js
@@ -0,0 +1,24 @@
+/*global io, $ */
+
+'use strict';
+
+var socket = io.connect('/');
+
+socket.on('message', function (data) {
+    data = JSON.parse(data);
+    $('#messages').append(
+        '<div class="' + data.type + '">' + data.message + '</div>'
+    );
+});
+
+$(document).ready(function () {
+    $('#send').click(function () {
+        var data = {
+            message: $('#message').val(),
+            type: 'userMessage'
+        };
+
+        socket.send(JSON.stringify(data));
+        $('#message').val('');
+    });
+});
