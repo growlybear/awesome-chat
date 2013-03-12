c30ccfb72b56de0317f98d20b1bb5f6ddd19b613
Michael Allan <michaelallan@optushome.com.au>
2013-03-12 21:43:31 +1100

    require a nickname from users, used to identify each message (styles rubbish)

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -4,14 +4,12 @@
 
 var socket = io.connect('/');
 
-socket.on('message', function (data) {
-    data = JSON.parse(data);
+socket.on('name_set', function (data) {
+    $('#nameform').hide();
     $('#messages').append(
-        '<div class="' + data.type + '">' + data.message + '</div>'
+        '<div class="systemMessage">Hello ' + data.name + '</div>'
     );
-});
 
-$(document).ready(function () {
     $('#send').click(function () {
         var data = {
             message: $('#message').val(),
@@ -20,5 +18,30 @@ $(document).ready(function () {
 
         socket.send(JSON.stringify(data));
         $('#message').val('');
+    });
+});
+
+socket.on('message', function (data) {
+    data = JSON.parse(data);
+
+    if (data.username) {
+        $('#messages').append(
+            '<div class="' + data.type + '"><span class="name">' +
+            data.username + ':</span> ' +
+            data.message + '</div>'
+        );
+    }
+    else {
+        $('#messages').append(
+            '<div class="' + data.type + '">' + data.message + '</div>'
+        );
+    }
+});
+
+$(document).ready(function () {
+    $('#setname').click(function () {
+        socket.emit('set_name', {
+            name: $('#nickname').val()
+        });
     });
 });
--- routes/sockets.js
+++ routes/sockets.js
@@ -8,19 +8,27 @@ exports.initialize = function (server) {
 
     io.sockets.on('connection', function (socket) {
 
-        socket.send(JSON.stringify({
-            type: 'serverMessage',
-            message: 'Welcome to the most interesting chat room on earth!'
-        }));
-
         socket.on('message', function (message) {
             message = JSON.parse(message);
 
             if (message.type === 'userMessage') {
-                socket.broadcast.send(JSON.stringify(message));
-                message.type = 'myMessage';
-                socket.send(JSON.stringify(message));
+                socket.get('nickname', function (err, nickname) {
+                    message.username = nickname;
+                    socket.broadcast.send(JSON.stringify(message));
+                    message.type = 'myMessage';
+                    socket.send(JSON.stringify(message));
+                });
             }
+        });
+
+        socket.on('set_name', function (data) {
+            socket.set('nickname', data.name, function () {
+                socket.emit('name_set', data);
+                socket.send(JSON.stringify({
+                    type: 'serverMessage',
+                    message: 'Welcome to the most interesting chat room on earth!'
+                }));
+            });
         });
     });
 };
